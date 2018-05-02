c0d754e0064a1b2aaba77f01b602fb2d130b7d92
Noushad Ali <ms.nowshadali@gmail.com>
2018-05-02 14:25:38 +0530

    Create config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const config = {
+  production: false,
+  alphaApiKey:'E1ILHV2EOEVWOTFF',
+  newsApiKey:'3ecae5441e034c17aa0eef816042c12a',
+  newsApiHeadlinesUrl:'https://newsapi.org/v2/top-headlines?country=',
+  googleAuthApiKey:'821905500436-di0qml19bemmcmfne1poegemgqf4dffh',
+  CHATURL:'ws://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=EUR&apikey=E1ILHV2EOEVWOTFF'
+};
