df6dd9142726937104a0e82bb8c86a957de645ea
Noushad Ali <ms.nowshadali@gmail.com>
2018-05-02 14:26:09 +0530

    Create config.prod.ts

---
+++ src/config/config.prod.ts
@@ -0,0 +1,8 @@
+export const configuration = {
+  production: false,
+  alphaApiKey:'E1ILHV2EOEVWOTFF',
+  newsApiKey:'3ecae5441e034c17aa0eef816042c12a',
+  newsApiHeadlinesUrl:'https://newsapi.org/v2/top-headlines?country=',
+  googleAuthApiKey:'821905500436-di0qml19bemmcmfne1poegemgqf4dffh',
+  CHATURL:'ws://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=EUR&apikey=E1ILHV2EOEVWOTFF'
+};
